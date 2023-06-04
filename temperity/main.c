835f799954bce9b6abf6db99c274dd5dbab50942
zakilive <zakilivebuzz.tech@gmail.com>
2023-06-04 15:59:08 +0200

    headers added

--- temperity/main.c
+++ temperity/main.c
@@ -1,4 +1,7 @@
 // Syed Ahmed Zaki - HIS Project-Group 4, Frankfurt University of Âpplied Science
+// This below code work with nrf2840dk boards pin P0.03(A0)
+// Some of the logic referred from riot os tests/dht
+
 #include <stdio.h>
 #include "xtimer.h"
 #include "timex.h"
@@ -12,7 +15,7 @@
 #include "ztimer.h"
 #include "periph/gpio.h"
 
-#define DELAY           (1 * US_PER_SEC)
+#define DELAY           (2 * US_PER_SEC)
 
 int main(void)
 {
@@ -22,18 +25,15 @@ int main(void)
 
     my_params.pin = GPIO_PIN(0,3);
 
-    puts("DHT temperature and humidity sensor test application\n");
-    // dht_params_t dht_type;
-    // gpio_init(GPIO_PIN(0,3),GPIO_IN);
-    // gpio_read(3);
+    puts("DHT temperature and humidity sensor application\n");
 
     /* initialize first configured sensor */
-    printf("Initializing DHT sensor...\t");
+    printf("Initialization status of DHT sensor...\t");
     if (dht_init(&dev1, &my_params) == DHT_OK) {
-        puts("[OK]\n");
+        puts("[Connected]\n");
     }
     else {
-        puts("[Failed]");
+        puts("[Connection Fails]");
         return 1;
     }
 
@@ -44,11 +44,11 @@ int main(void)
             continue;
         }
 
-        printf("DHT values - temp: %d.%d°C - relative humidity: %d.%d%%\n",
+        printf("DHT values - temperature: %d.%d°C - relative humidity: %d.%d%%\n",
                temp/10, temp%10, hum/10, hum%10);
 
         xtimer_usleep(DELAY);
     }
 
     return 0;
-}
+}W
