1b475db9ed2ee9ae8e834f0b96a84bd469cf17f3
zakilive <zakilivebuzz.tech@gmail.com>
2023-06-16 09:02:38 +0200

    bin removed

--- saul_temperature/bin/native/cpu/tramp.d
+++
@@ -1,6 +0,0 @@
-/home/zakilive/Desktop/riot-final/RIOT/examples/saul_temperature/bin/native/cpu/tramp.o: \
- /home/zakilive/Desktop/riot-final/RIOT/cpu/native/tramp.S \
- /usr/include/stdc-predef.h \
- /home/zakilive/Desktop/riot-final/RIOT/examples/saul_temperature/bin/native/riotbuild/riotbuild.h
-/usr/include/stdc-predef.h:
-/home/zakilive/Desktop/riot-final/RIOT/examples/saul_temperature/bin/native/riotbuild/riotbuild.h:
