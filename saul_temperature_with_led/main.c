4b2373f2fa60d51ff9a5c2a7623ff2a23c9fd035
zakilive <zakilivebuzz.tech@gmail.com>
2023-06-23 16:00:44 +0200

    SAUL with DHT and LED

--- saul_temperature_with_led/main.c
+++ saul_temperature_with_led/main.c
@@ -1,62 +1,53 @@
 #include <stdio.h>
-#include <saul.h>
-#include"saul_reg.h"
+#include "dht.h"
+#include "saul_reg.h"
+#include "saul.h"
 
-#include "clk.h"
-#include "board.h"
-#include "periph_conf.h"
-#include "timex.h"
+#include "time_units.h"
 #include "ztimer.h"
+
 #include "periph/gpio.h"
+#include "periph_conf.h"
 
 #define ON 1
 #define OFF 0
 
-static void delay(void)
+static void _print_led(void) {
+	gpio_init(GPIO_PIN(0,4),GPIO_OUT);
+
+   	gpio_write(GPIO_PIN(0,4),ON);
+	ztimer_sleep(ZTIMER_SEC, 1); //need timer for delay
+	gpio_write(GPIO_PIN(0,4),OFF);
+	ztimer_sleep(ZTIMER_SEC, 1); //need timer for delay
+}
+
+static void _dht_func(void) {
+	static saul_reg_t *saul_dht;
+	static phydat_t result;
+	saul_dht = saul_reg_find_name("dht");
+
+		
+	
+	while(1) {
+		int res = saul_reg_read(saul_dht, &result); //need to put it in while loop to get data sequencially
+		int temp_val = result.val[0]/10;
+	
+		if (res) {
+			printf("temperature: %d C\n", temp_val); //newline \n is needful to print it
+			if (temp_val > 23) { //if environment temperature is greater than 23 light will blink
+				_print_led();
+			}
+		} else {
+			printf("Sensor read failure: %d\n", res);
+		}		
+		
+		ztimer_sleep(ZTIMER_USEC, 5); //need timer for delay
+	}
+}
+
+int main(void)
 {
-    if (IS_USED(MODULE_ZTIMER)) {
-        ztimer_sleep(ZTIMER_USEC, 1 * US_PER_SEC);
-    }
-    else {
-        uint32_t loops = coreclk() / 20;
-        for (volatile uint32_t i = 0; i < loops; i++) { }
-    }
-}
+	_dht_func();
 
-static void print_led(void) {
-    gpio_init(GPIO_PIN(0,3),GPIO_OUT);
-
-   	gpio_write(GPIO_PIN(0,3),ON);
-	delay();
-	gpio_write(GPIO_PIN(0,3),OFF);
-
-}
-
-static void temp_saul(void) {
-    int sensor_status;
-
-   	saul_reg_t *dev = saul_reg_find_nth(8);
-	phydat_t result;
-
-    sensor_status=saul_reg_read(dev, &result);
-    puts("Sensor Status");
-
-    while(1) {
-        if (sensor_status >= 0) {
-            int temperature_celsius=result.val[0]/100;
-            printf("Sensor data: %d\n", temperature_celsius);  // Print the sensor data
-            if (temperature_celsius>29) {
-                print_led();
-            }
-
-        } else {
-            printf("Failed to read sensor data\n");
-        }
-    }
-}
-
-int main(void) {
-	temp_saul();
-
-    return 0;
+	return 0;
 }
